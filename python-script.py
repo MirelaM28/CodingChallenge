f297cf887274f6d6aaf593a1ba2ba6f7308ba2a1
MirelaM28 <mirela_musuroi@yahoo.com>
2025-05-17 19:50:34 +0300

    Added the duration of each task

--- python-script.py
+++ python-script.py
@@ -17,7 +17,6 @@ def retrieve_tasks_ids(input_file):
                tasks_ids=list(set(re.findall(r'(?<=task )(\w+)',tasks)))
    return tasks_ids
 
-#Function that will get for each task id the start time, end time
 def start_end_tasks(input_file, task_ids):
     results=[]
     with open(input_file, 'r') as file:
@@ -32,10 +31,28 @@ def start_end_tasks(input_file, task_ids):
                     elif 'END' in line:
                         end_task=line.split(',')
 
-            results.append((task_id,start_task,end_task))
+#Added the calculation of tasks
+
+            if start_task:
+                start_seconds=int(datetime.strptime(start_task[0], "%H:%M:%S").timestamp())
+
+            if end_task:
+                end_seconds=int(datetime.strptime(end_task[0], "%H:%M:%S").timestamp())
+                difference=end_seconds-start_seconds
+                hours=difference//3600
+                minutes=(difference % 3600) // 60
+                seconds=difference % 60
+            else:
+                end_task=["NOT ENDED"]
+                hours=0
+                minutes=0
+                seconds=0
+
+#Keeped only the first element of start_task & end_task strings since the first value reflects the time 
+            results.append((task_id,start_task[0],end_task[0],f"{hours}:{minutes}:{seconds}"))
 
         return results
 
-task_id=retrieve_tasks_ids(input_file)
-print(start_end_tasks(input_file,task_id))
+output_first_function=retrieve_tasks_ids(input_file)
+print(start_end_tasks(input_file,output_first_function))
 
